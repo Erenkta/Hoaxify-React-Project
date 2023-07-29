79bb41d30e95bc6479d27fd9957eb9e2df132a4d
Eren Nokta <eren.nkta@gmail.com>
2023-07-29 12:36:40 +0300

    React Router Commited
    Npm install sırasında biraz fazla hata çıktığı için eski dosyalarla yenilerini değiştirmek zorunda kaldım bundan dolayı alakasız bir kaç yerde değişiklik olabilir. Commit'in açıklamasına gelecek olursak react-router-dom ile sayfalar arası geçişi düzgün bir biçimde sağladık. Ve componentWillUnmount kullanarak yığılmayı engelledik

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { StrictMode } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './bootstrap-override.scss';
@@ -6,27 +6,9 @@ import * as serviceWorker from './serviceWorker';
 import './i18n';
 import App from './container/App';
 
-
-
-ReactDOM.render(
-  <div>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </div>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-/*
-    <ApiProgress>
-      <LoginPage />   Bunu böyle yapmamızın sebebi Parent-Child ilişkisi kurduk artık Parent->Child'a parametre yollayabiliriz
-    </ApiProgress>
-
-
-    strict mode'u kendi ekletti
-*/
