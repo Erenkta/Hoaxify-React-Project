39abd8273339c553ac3b0c6065d9bb13c0c4722a
Eren Nokta <eren.nkta@gmail.com>
2023-07-29 13:30:58 +0300

    Topbar Global State Commited
    Topbar'ın durumları commitlendi. Mesela  artık giriş yapınca orda username ve logout yazıyor. Logout'a basınca ise Login ve Signup oluyor. Lifting State Up yöntemi ile state'ler App'ten topbar ve login page'e yollandı. Artık kullanıcı giriş yapmışken url aracılığıyla Login ve Signup page'e ulaşamıyor

--- src/container/App.js
+++ src/container/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import UserSignupPage from '../pages/UserSignupPage';
 import LoginPage from '../pages/LoginPage';
 import LanguageSelector from '../components/LanguageSelector';
@@ -6,23 +6,60 @@ import HomePage from '../pages/HomePage';
 import UserPage from '../pages/UserPage';
 import { HashRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import TopBar from '../components/TopBar';
+class App extends Component {
 
-function App() {
-  return (
-    <div>
-      <Router>
-        <TopBar />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/signup" component={UserSignupPage} />
-          <Route path="/user/:username" component={UserPage} />
-          <Redirect to="/" />
-        </Switch>
-      </Router>
-      <LanguageSelector />
-    </div>
-  );
+  state = {
+    isLoggedIn: false,
+    username: undefined,
+  }
+
+  onLoginSuccess = (username) => {
+    this.setState({
+      username: username,
+      isLoggedIn: true
+    })
+  }
+  onLogoutSuccess = () => {
+    this.setState({
+      isLoggedIn: false,
+      username: undefined,
+    })
+  }
+
+  render() {
+    const { isLoggedIn, username } = this.state //Burada pasladık
+    return (
+      <div>
+        <Router>
+          <TopBar username={username} isLoggedIn={isLoggedIn} onLogoutSuccess={this.onLogoutSuccess} />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            {!isLoggedIn && (< Route path="/login" component={(props) => { return <LoginPage {...props} onLoginSuccess={this.onLoginSuccess} /> }} />)}
+            {!isLoggedIn && (<Route path="/signup" component={UserSignupPage} />)}
+            <Route path="/user/:username" component={UserPage} />
+            <Redirect to="/" />
+          </Switch>
+        </Router>
+        <LanguageSelector />
+      </div>
+    );
+  }
 }
 
 export default App;
+
+/*
+ <Route path="/login" component={(reactRouterProps) => { return <LoginPage {...reactRouterProps} /> }} />
+
+ Burada amaç Bize Route'un verdiği property'leri Login page'e olduğu gibi vermek
+ zaten bu default olarak da veriliyordu ama biz başka bir şeyler de pass'layacağımız için bunu vermemiz lazımdı
+
+ onLoginSuccess={this.onLoginSuccess} bu da login success olunca yapılacak bir şey istiyordu biz de dedik ki bunu çalıştır
+
+
+
+ Sorun şu ki biz giriş yapsak bile hala url kısmına Login yazarak login page'i açabiliyoruz
+ bunu düzeltmek için  bir kaç işlem yapıcaz
+          {!isLoggedIn && < Route path="/login" component={(props) => { return <LoginPage {...props} onLoginSuccess={this.onLoginSuccess} /> }} />}
+Böyle bir conditional belirleyerek yapabiliriz
+*/
