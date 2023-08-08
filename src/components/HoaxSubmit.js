7888510c09a1bc7df431ef0be64b7a829282b04e
Eren Nokta <eren.nkta@gmail.com>
2023-08-08 15:42:32 +0300

    Hoax Validation and Pending API call commited
    Validation'ları backend'de hallettikten sonra client kısmında errorları gostermeyi ve kontrolleri sağladık. Ayrıca Devam eden bir api işlemi varsa button'da spinner gösterdik ve cancel tuşunu disabled ettik

--- src/components/HoaxSubmit.js
+++ src/components/HoaxSubmit.js
@@ -5,6 +5,8 @@ import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useEffect } from 'react';
 import { postHoax } from '../api/apiCalls';
+import { useApiProgress } from '../shared/ApiProgress';
+import ButtonWithProgress from './ButtonWithProgress';
 
 const HoaxSubmit = () => {
     const { image } = useSelector((store) => {
@@ -14,12 +16,25 @@ const HoaxSubmit = () => {
     })
     const [focused, setFocused] = useState(false)
     const [hoax, setHoax] = useState(' ')
+    const [errors, setErrors] = useState({})
+
+
+
     const { t } = useTranslation();
     useEffect(() => {
         if (!focused) {
-            setHoax(' ')
+            setHoax('')
+            setErrors({})
         }
     }, [focused])
+    useEffect(() => {
+        setErrors((previousError) => {
+            return {
+                ...previousError,
+                content: undefined
+            }
+        })
+    }, [hoax])
 
 
     const onClickSubmit = async () => {
@@ -28,22 +43,42 @@ const HoaxSubmit = () => {
         }
         try {
             await postHoax(body)
-        } catch (error) { }
+            setFocused(false)
+
+        } catch (error) {
+            if (error.response.data.validationErrors) {
+                setErrors(error.response.data.validationErrors) //Maksat kendi state'imizde güncelledik
+            }
+        }
     }
+    let textAreaClass = 'form-control'
+    if (errors.content) {
+        textAreaClass += ' is-invalid'
+    }
+
+    const pendingApiCall = useApiProgress('post', '/api/1.0/hoaxes')
     return (
         <div className='card p-1 flex-row'>
             <ProfileImageWithDefault image={image} width="32" height="32" className="rounded-circle mr-2" />
             <div className='flex-fill'>
                 <textarea
-                    className='form-control '
+                    className={textAreaClass}
                     rows={focused ? "3" : "1"}
                     onFocus={() => { setFocused(true) }}
                     onChange={(event) => { setHoax(event.target.value) }}
                     value={hoax} />
+                <div className='invalid-feedback'>{errors.content}</div>
                 {focused && <div className='text-right mt-1'>
-                    <button className='btn btn-primary' onClick={onClickSubmit}>Hoaxify</button>
+                    <ButtonWithProgress
+                        text={'Hoaxify'}
+                        className='btn btn-primary'
+                        onClick={onClickSubmit}
+                        pendingApiCall={pendingApiCall}
+                        disabled={pendingApiCall}
+                    />
                     <button className='btn btn-light  d-inline-flex'
-                        onClick={() => { setFocused(false) }}>
+                        onClick={() => { setFocused(false) }}
+                        disabled={pendingApiCall}>
                         <i className='material-icons'>close</i>{t('Cancel')}
                     </button>
                 </div>}
