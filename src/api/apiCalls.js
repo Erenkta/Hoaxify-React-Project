72602008308dd7ef1e44c9c3774a1b4c49ebaf1f
Eren Nokta <eren.nkta@gmail.com>
2023-08-15 11:28:22 +0300

    Hoax-Attachment commited
    File yüklerken devam eden api işlemleri için bir spinner çıkardık ve koyduğumuz fotoğraf üzerinde gösterdik. Bir kaç görsel ve kullanıcıyı bilgilendiren işlemler yaptık

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -8,57 +8,55 @@ export const login = creds => {
   return axios.post('/api/1.0/auth', {}, { auth: creds });
 };
 
-export const getUsers = (page = 0, size = 3) => {
-  return axios.get(`/api/1.0/users?currentPage=${page}&pageSize=${size}`) // bir body belirtmeye gerek yok
-}
-
-export const getUserByUsername = username => {
-  return axios.get(`/api/1.0/users/${username}`)
-}
-
-export const updateUser = (username, body) => {
-  return axios.put(`/api/1.0/users/${username}`, body)
-}
-
-export const postHoax = hoax => {
-  return axios.post('/api/1.0/hoaxes', hoax)
-}
-
-export const getHoaxes = (username, currentPage = 0) => {
-  const path = username ? `/api/1.0/users/${username}/hoaxes/?currentPage=${currentPage}` : `/api/1.0/hoaxes/?currentPage=${currentPage}`
-  return axios.get(path)
-}
-export const getOldHoaxes = (id, username) => {
-  const path = username ? `/api/1.0/users/${username}/hoaxes/${id}` : `/api/1.0/hoaxes/${id}`
-  return axios.get(path)
-}
-export const getNewHoaxes = (id, username) => {
-  const path = username ? `/api/1.0/users/${username}/hoaxes/${id}?direction=after` : `/api/1.0/hoaxes/${id}?direction=after`
-  return axios.get(path)
-}
-
-
-export const getNewHoaxCount = (id, username) => {
-  const path = username ? `/api/1.0/users/${username}/hoaxes/${id}?count=true` : `/api/1.0/hoaxes/${id}?count=true`
-  return axios.get(path);
-}
-
-
-
-
-
-
-
 export const changeLanguage = language => {
   axios.defaults.headers['accept-language'] = language;
 };
 
+export const getUsers = (page = 0, size = 3) => {
+  return axios.get(`/api/1.0/users?page=${page}&size=${size}`);
+};
+
 export const setAuthorizationHeader = ({ username, password, isLoggedIn }) => {
   if (isLoggedIn) {
-    const authorizationHeaderValue = `Basic ${btoa(username + ':' + password)}` //Base 64 haline çevirdik
-    axios.defaults.headers['Authorization'] = authorizationHeaderValue
+    const authorizationHeaderValue = `Basic ${btoa(username + ':' + password)}`;
+    axios.defaults.headers['Authorization'] = authorizationHeaderValue;
   } else {
-    delete axios.defaults.headers['Authorization']
+    delete axios.defaults.headers['Authorization'];
   }
+};
 
-}
+export const getUser = username => {
+  return axios.get(`/api/1.0/users/${username}`);
+};
+
+export const updateUser = (username, body) => {
+  return axios.put(`/api/1.0/users/${username}`, body);
+};
+
+export const postHoax = hoax => {
+  return axios.post('/api/1.0/hoaxes', hoax);
+};
+
+export const getHoaxes = (username, page = 0) => {
+  const path = username ? `/api/1.0/users/${username}/hoaxes?page=` : '/api/1.0/hoaxes?page=';
+  return axios.get(path + page);
+};
+
+export const getOldHoaxes = (id, username) => {
+  const path = username ? `/api/1.0/users/${username}/hoaxes/${id}` : `/api/1.0/hoaxes/${id}`;
+  return axios.get(path);
+};
+
+export const getNewHoaxCount = (id, username) => {
+  const path = username ? `/api/1.0/users/${username}/hoaxes/${id}?count=true` : `/api/1.0/hoaxes/${id}?count=true`;
+  return axios.get(path);
+};
+
+export const getNewHoaxes = (id, username) => {
+  const path = username ? `/api/1.0/users/${username}/hoaxes/${id}?direction=after` : `/api/1.0/hoaxes/${id}?direction=after`;
+  return axios.get(path);
+};
+
+export const postHoaxAttachment = attachment => {
+  return axios.post('/api/1.0/hoax-attachments', attachment);
+};
--- src/components/HoaxSubmit.js
+++ src/components/HoaxSubmit.js
@@ -1,91 +1,109 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import ProfileImageWithDefault from './ProfileImageWithDefault';
-import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useEffect } from 'react';
-import { postHoax } from '../api/apiCalls';
+import { postHoax, postHoaxAttachment } from '../api/apiCalls';
 import { useApiProgress } from '../shared/ApiProgress';
 import ButtonWithProgress from './ButtonWithProgress';
+import Input from './Input';
+import AutoUploadImage from './AutoUploadImage';
 
 const HoaxSubmit = () => {
-    const { image } = useSelector((store) => {
-        return {
-            image: store.image
-        }
-    })
-    const [focused, setFocused] = useState(false)
-    const [hoax, setHoax] = useState(' ')
-    const [errors, setErrors] = useState({})
+  const { image } = useSelector(store => ({ image: store.image }));
+  const [focused, setFocused] = useState(false);
+  const [hoax, setHoax] = useState('');
+  const [errors, setErrors] = useState({});
+  const [newImage, setNewImage] = useState();
+  const { t } = useTranslation();
 
-
-
-    const { t } = useTranslation();
-    useEffect(() => {
-        if (!focused) {
-            setHoax('')
-            setErrors({})
-        }
-    }, [focused])
-    useEffect(() => {
-        setErrors((previousError) => {
-            return {
-                ...previousError,
-                content: undefined
-            }
-        })
-    }, [hoax])
-
-
-    const onClickSubmit = async () => {
-        const body = {
-            content: hoax
-        }
-        try {
-            await postHoax(body)
-            setFocused(false)
-
-        } catch (error) {
-            if (error.response.data.validationErrors) {
-                setErrors(error.response.data.validationErrors) //Maksat kendi state'imizde güncelledik
-            }
-        }
+  useEffect(() => {
+    if (!focused) {
+      setHoax('');
+      setErrors({});
+      setNewImage();
     }
-    let textAreaClass = 'form-control'
-    if (errors.content) {
-        textAreaClass += ' is-invalid'
-    }
+  }, [focused]);
 
-    const pendingApiCall = useApiProgress('post', '/api/1.0/hoaxes')
-    return (
-        <div className='card p-1 flex-row'>
-            <ProfileImageWithDefault image={image} width="32" height="32" className="rounded-circle mr-2" />
-            <div className='flex-fill'>
-                <textarea
-                    placeholder='Hoax Here Now!'
-                    className={textAreaClass}
-                    rows={focused ? "3" : "1"}
-                    onFocus={() => { setFocused(true) }}
-                    onChange={(event) => { setHoax(event.target.value) }}
-                    value={hoax} />
-                <div className='invalid-feedback'>{errors.content}</div>
-                {focused && <div className='text-right mt-1'>
-                    <ButtonWithProgress
-                        text={'Hoaxify'}
-                        className='btn btn-primary'
-                        onClick={onClickSubmit}
-                        pendingApiCall={pendingApiCall}
-                        disabled={pendingApiCall}
-                    />
-                    <button className='btn btn-light  d-inline-flex'
-                        onClick={() => { setFocused(false) }}
-                        disabled={pendingApiCall}>
-                        <i className='material-icons'>close</i>{t('Cancel')}
-                    </button>
-                </div>}
+  useEffect(() => {
+    setErrors({});
+  }, [hoax]);
+
+  const pendingApiCall = useApiProgress('post', '/api/1.0/hoaxes', true);
+  const pendingFileUpload = useApiProgress('post', '/api/1.0/hoax-attachments', true)
+
+  const onClickHoaxify = async () => {
+    const body = {
+      content: hoax
+    };
+
+    try {
+      await postHoax(body);
+      setFocused(false);
+    } catch (error) {
+      if (error.response.data.validationErrors) {
+        setErrors(error.response.data.validationErrors);
+      }
+    }
+  };
+
+  const onChangeFile = event => {
+    if (event.target.files.length < 1) {
+      return;
+    }
+    const file = event.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.onloadend = () => {
+      setNewImage(fileReader.result);
+      uploadFile(file);
+    };
+    fileReader.readAsDataURL(file);
+  };
+
+  const uploadFile = async file => {
+    const attachment = new FormData();
+    attachment.append('file', file);
+    await postHoaxAttachment(attachment);
+  };
+
+  let textAreaClass = 'form-control';
+  if (errors.content) {
+    textAreaClass += ' is-invalid';
+  }
+
+  return (
+    <div className="card p-1 flex-row">
+      <ProfileImageWithDefault image={image} width="32" height="32" className="rounded-circle mr-1" />
+      <div className="flex-fill">
+        <textarea
+          className={textAreaClass}
+          rows={focused ? '3' : '1'}
+          onFocus={() => setFocused(true)}
+          onChange={event => setHoax(event.target.value)}
+          value={hoax}
+        />
+        <div className="invalid-feedback">{errors.content}</div>
+        {focused && (
+          <>
+            {!newImage && <Input type="file" onChange={onChangeFile} />}
+            {newImage && <AutoUploadImage image={newImage} uploading={pendingFileUpload} />}
+            <div className="text-right mt-1">
+              <ButtonWithProgress
+                className="btn btn-primary"
+                onClick={onClickHoaxify}
+                text="Hoaxify"
+                pendingApiCall={pendingApiCall}
+                disabled={pendingApiCall || pendingFileUpload}
+              />
+              <button className="btn btn-light d-inline-flex ml-1" onClick={() => setFocused(false)} disabled={pendingApiCall || pendingFileUpload}>
+                <i className="material-icons">close</i>
+                {t('Cancel')}
+              </button>
             </div>
-        </div>
-    );
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default HoaxSubmit;
